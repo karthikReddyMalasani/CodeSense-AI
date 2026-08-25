5cb0c85685fb02c9ee9632afe4a40cd0a9b7a4d5
Karthik kumar reddy Malasani <239x1a3360@gprec.ac.in>
2026-08-25 23:42:45 +0530

    Update WebConfig.java

--- codesense-ai/backend/src/main/java/com/codesense/common/config/WebConfig.java
+++ codesense-ai/backend/src/main/java/com/codesense/common/config/WebConfig.java
@@ -6,11 +6,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOriginPatterns("http://localhost:3000", "http://localhost:3001", "${REACT_APP_BASE_URL:*}")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedOriginPatterns(
+                        "http://localhost:3000",
+                        "http://localhost:3001",
+                        "https://codesense-ai-ibm.vercel.app"
+                )
+                .allowedMethods(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "DELETE",
+                        "PATCH",
+                        "OPTIONS"
+                )
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
